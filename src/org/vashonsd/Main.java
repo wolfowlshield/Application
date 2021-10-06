9e2a8cee5b7d1e045f59e0dbea58ec2c83940aa2
wolfowlshield <btouch12@gmail.com>
2021-10-06 10:40:31 -0700

    Initial commit

---
+++ src/org/vashonsd/Main.java
@@ -0,0 +1,12 @@
+package org.vashonsd;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(numSquared(4));
+    }
+
+    private static int numSquared(int num) {
+        return num * num;
+    }
+}
